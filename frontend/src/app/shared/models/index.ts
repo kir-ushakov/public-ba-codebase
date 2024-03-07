40f5791126791328a6560088963d0aa63c5f32e4
kir-ushakov <kitushakoff@gmail.com>
2024-03-07 11:50:34 +0300

    removed the hurried note

--- frontend/src/app/shared/models/index.ts
+++ frontend/src/app/shared/models/index.ts
@@ -1,10 +1,3 @@
-/**
- * #NOTE
- * The model folder has to contain only interfaces of object
- * that important for application in general,
- * not only for specific component of service.
- * They should be a part of business logic of application.
- */
 import { User } from './user.model';
 import { Task, ETaskStatus, ETaskType } from './task.model';
 import { Change, EChangeType, ChangeableObject } from './change.model';
