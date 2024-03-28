8abf59ebba2899cd0e3ae1323ff31748e2580673
kir-ushakov <kitushakoff@gmail.com>
2024-03-28 19:21:54 +0300

    added MbProfileScreenAction

---
+++ frontend/src/app/mobile-app/components/screens/mb-profile-screen/mb-profile-screen.actions.ts
@@ -0,0 +1,9 @@
+export namespace MbProfileScreenAction {
+  export class Logout {
+    static readonly type = '[MbProfileScreen] Logout';
+  }
+
+  export class RemoveFromSlack {
+    static readonly type = '[MbProfileScreen] Remove From Slack';
+  }
+}
