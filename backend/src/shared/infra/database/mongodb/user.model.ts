49bb2306f9d2a3c904c74e2484f1a92939a54234
kir-ushakov <kitushakoff@gmail.com>
2024-04-16 17:02:58 +0300

    added user model

---
+++ backend/src/shared/infra/database/mongodb/user.model.ts
@@ -0,0 +1,41 @@
+import mongoose, {
+  PassportLocalDocument,
+  PassportLocalModel,
+  Schema,
+} from 'mongoose';
+import passportLocalMongoose from 'passport-local-mongoose';
+import { UserPersistent } from '../../../domain/models/user';
+import VerificationTokenModel, {
+  VerificationTokenDocumnet,
+} from './verification-token.model';
+
+const crypto = require('crypto');
+
+export interface UserDocument extends UserPersistent, PassportLocalDocument {
+  generateVerificationToken: () => VerificationTokenDocumnet;
+}
+
+const UserSchema = new Schema({
+  firstName: { type: String, require: true },
+  lastName: { type: String, require: true },
+  username: { type: String, require: true },
+  verified: { type: Boolean, required: true, default: false },
+  googleId: { type: String, required: false },
+});
+
+// attach passportLocalMongoose functionality to UserScheme
+UserSchema.plugin(passportLocalMongoose);
+
+UserSchema.methods.generateVerificationToken = function () {
+  let payload = {
+    userId: this._id,
+    token: crypto.randomBytes(20).toString('hex'),
+  };
+
+  return new VerificationTokenModel(payload);
+};
+
+const UserModel: PassportLocalModel<UserDocument> =
+  mongoose.model<UserDocument>('User', UserSchema);
+
+export default UserModel;
