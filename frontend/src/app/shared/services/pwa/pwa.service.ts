c7cdf893bf48227c3955337bb300d1819bb25a88
Kirill Ushakov <kitushakoff@gmail.com>
2025-03-04 20:14:56 +0300

    PwaService added

---
+++ frontend/src/app/shared/services/pwa/pwa.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+@Injectable()
+export class PwaService {
+  constructor(private swUpdate: SwUpdate) {
+    this.swUpdate.versionUpdates.subscribe((event) => {
+      if (event.type === 'VERSION_READY') {
+        window.location.reload();
+      }
+    });
+  }
+}
