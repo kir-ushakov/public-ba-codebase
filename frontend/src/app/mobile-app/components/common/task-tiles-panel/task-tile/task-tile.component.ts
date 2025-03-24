512a433447ea6a8c8049a7ecc10b0c409e732a29
Kirill Ushakov <kitushakoff@gmail.com>
2025-03-24 11:05:29 +0300

    reqest tumbs with width for optimization

--- frontend/src/app/mobile-app/components/common/task-tiles-panel/task-tile/task-tile.component.ts
+++ frontend/src/app/mobile-app/components/common/task-tiles-panel/task-tile/task-tile.component.ts
@@ -1,20 +1,33 @@
-import { Component, Input, HostListener } from '@angular/core';
+import { Component, Input, HostListener, signal, ViewChild, ElementRef, Signal, WritableSignal } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { MbTaskTileAction } from './task-tile.actions';
 import { Task } from 'src/app/shared/models/task.model';
 import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from 'src/app/shared/components/ui-elements/spinner/spinner.component';
 
 @Component({
     selector: 'ba-task-tile',
     templateUrl: './task-tile.component.html',
     styleUrls: ['./task-tile.component.scss'],
-    imports: [ CommonModule ]
+    imports: [ CommonModule, SpinnerComponent ]
 })
 export class TaskTileComponent {
   @Input() task: Task;
   @HostListener('click') onClick() {
     this.store.dispatch(new MbTaskTileAction.Clicked(this.task.id));
   }
+  @ViewChild(SpinnerComponent, { static: false }) spinnerComponent!: SpinnerComponent;
 
-  constructor(private store: Store) {}
+  DEFAULT_IMAGE_WIDTH = 50;
+
+  isLoading = signal(true);
+  resizedImageUri: WritableSignal<null | string> = signal(null);
+
+  constructor(private store: Store) { }
+
+  ngAfterViewInit() {
+    const width = this.spinnerComponent?.getNativeElement()?.offsetWidth || this.DEFAULT_IMAGE_WIDTH;
+    const dpr = window.devicePixelRatio || 1;
+    this.resizedImageUri.set(this.task.imageUri + `?width=${dpr * width}`);
+  }
 }
