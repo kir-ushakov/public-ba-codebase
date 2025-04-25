54560129efd958e0e3ea2e3618cf8ef214918d06
Kirill Ushakov <kitushakoff@gmail.com>
2025-04-25 19:19:58 +0300

    refactoring of API error DTO

--- backend/src/shared/infra/http/dtos/api-errors.dto.ts
+++ backend/src/shared/infra/http/dtos/api-errors.dto.ts
@@ -1,8 +1,4 @@
-import { EAppErrors } from '../../../core/app-error.enum.js';
-
-// TODO: Need to remove this interface
-// TOCKET: https://brainas.atlassian.net/browse/BA-117
 export interface IApiErrorDto {
-  name: EAppErrors;
+  name: string;
   message: string;
 }
