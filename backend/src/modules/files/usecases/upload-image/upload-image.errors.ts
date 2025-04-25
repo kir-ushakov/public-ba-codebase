84715e2c2bcf54a594c9e0c3d6e515ef6183cf06
Kirill Ushakov <kitushakoff@gmail.com>
2025-04-25 19:31:23 +0300

    UploadToGoogleDriveFailed usecase error added

--- backend/src/modules/files/usecases/upload-image/upload-image.errors.ts
+++ backend/src/modules/files/usecases/upload-image/upload-image.errors.ts
@@ -3,7 +3,8 @@ import { UseCaseError } from '../../../../shared/core/use-case-error.js';
 import { EHttpStatus } from '../../../../shared/infra/http/models/base-controller.js';
 
 export enum EUploadImageError {
-  NotSupportedType = 'NOT_SUPPORTED_TYPE',
+  NotSupportedType = 'UPLOAD_IMAGE_ERROR__NOT_SUPPORTED_TYPE',
+  UploadToGoogleDriveFailed = 'UPLOAD_IMAGE_ERROR__UPLOAD_TO_GOOGLE_DRIVE_FAILED',
 }
 
 export namespace UploadImageErrors {
@@ -14,8 +15,21 @@ export namespace UploadImageErrors {
         new UseCaseError(
           EHttpStatus.BadRequest,
           EUploadImageError.NotSupportedType,
-          `The file type "${type}" doesn't supported`
-        )
+          `The file type "${type}" doesn't supported`,
+        ),
+      );
+    }
+  }
+
+  export class UploadToGoogleDriveFailed extends Result<UseCaseError> {
+    constructor() {
+      super(
+        false,
+        new UseCaseError(
+          EHttpStatus.BadGateway,
+          EUploadImageError.UploadToGoogleDriveFailed,
+          `Uploading file to Google Drive failed`,
+        ),
       );
     }
   }
