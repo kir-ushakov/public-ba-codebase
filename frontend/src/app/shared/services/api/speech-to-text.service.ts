6ff92bd77a69c3a7e612fd459859c1963629e385
Kirill Ushakov <kitushakoff@gmail.com>
2025-05-12 12:06:55 +0300

    SpeechToTextService

---
+++ frontend/src/app/shared/services/api/speech-to-text.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface SpeechToTextResponse {
+  transcript: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class SpeechToTextService {
+  private readonly VOICE_DECODE_API_ENDPOINT = `${environment.baseUrl}/ai/speech-to-text`;
+  constructor(private http: HttpClient) {}
+
+  uploadAudio(blob: Blob): Observable<{ transcript: string }> {
+    const formData = new FormData();
+    formData.append('file', blob, 'voice.webm');
+
+    return this.http.post<{ transcript: string }>(this.VOICE_DECODE_API_ENDPOINT, formData);
+  }
+}
