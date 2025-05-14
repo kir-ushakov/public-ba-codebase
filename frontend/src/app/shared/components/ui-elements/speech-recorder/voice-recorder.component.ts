8de2f676f7718225750d2ab881468898a57cdb96
Kirill Ushakov <kitushakoff@gmail.com>
2025-05-14 09:49:26 +0300

    refactoring of voice recorder component

--- frontend/src/app/shared/components/ui-elements/speech-recorder/voice-recorder.component.ts
+++ frontend/src/app/shared/components/ui-elements/speech-recorder/voice-recorder.component.ts
@@ -3,33 +3,42 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MicIconComponent } from './mic-icon/mic-icon.component';
+import { CommonModule } from '@angular/common';
 
 const DEFAULT_MIC_COLOR = 'rgba(0, 255, 0, 0.7)';
+const CIRCLE_RADIUS = 147;
+const RECORDING_DURATION = 10000;
 
 @Component({
   selector: 'ba-voice-recorder',
-  imports: [MatIconModule, MatButtonModule, MicIconComponent],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MicIconComponent],
   templateUrl: './voice-recorder.component.html',
   styleUrl: './voice-recorder.component.scss',
 })
 export class VoiceRecorderComponent {
   @ViewChild('progressCircle', { static: false }) circleRef!: ElementRef<SVGCircleElement>;
 
+  radius = CIRCLE_RADIUS;
   micColor = signal(DEFAULT_MIC_COLOR);
+
+  get radiusCssValue(): string {
+    return `clamp(60px, 40vw, ${this.radius}px)`;
+  }
+
+  get micSizeCssValue(): string {
+    return `clamp(60px, 40vw, ${this.radius / 2}px)`;
+  }
 
   constructor(private dialogRef: MatDialogRef<VoiceRecorderComponent>) {}
 
   private animationFrame: number | null = null;
 
-  startRecording(duration = 10000) {
-    const circle = this.circleRef.nativeElement;
+  ngOnInit() {}
 
-    const radius = 47;
-    const totalLength = 2 * Math.PI * radius; // = ~295.31
-    circle.style.strokeDasharray = `${totalLength}`;
-    circle.style.strokeDashoffset = `${totalLength}`;
+  ngAfterViewInit() {}
 
-    this.animateProgress(circle, duration, totalLength);
+  startRecording(duration = RECORDING_DURATION) {
+    this.animateProgress(duration);
   }
 
   stopRecording() {
@@ -47,7 +56,12 @@ export class VoiceRecorderComponent {
     this.stopRecording();
   }
 
-  private animateProgress(circle: SVGCircleElement, duration: number, totalLength: number) {
+  private animateProgress(duration: number) {
+    const circle = this.circleRef.nativeElement;
+    const totalLength = 2 * Math.PI * this.radius;
+    circle.style.strokeDasharray = `${totalLength}`;
+    circle.style.strokeDashoffset = `${totalLength}`;
+
     const start = performance.now();
 
     const animate = (now: number) => {
@@ -57,7 +71,7 @@ export class VoiceRecorderComponent {
       // Dash offset
       circle.style.strokeDashoffset = progress >= 1 ? '0' : `${totalLength * (1 - progress)}`;
 
-      // Color interpolation (yellow → red)
+      // Color interpolation (green → red)
       const red = Math.round(255 * progress);
       const green = Math.round(255 * (1 - progress));
       circle.style.stroke = `rgb(${red}, ${green}, 0)`;
