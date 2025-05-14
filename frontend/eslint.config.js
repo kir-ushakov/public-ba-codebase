bfd7f188997374ea44e3d60b05d68fc4e9c85d21
Kirill Ushakov <kitushakoff@gmail.com>
2025-05-14 17:40:03 +0300

    added some rules

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,6 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import angularPlugin from '@angular-eslint/eslint-plugin';
 import angularTemplatePlugin from '@angular-eslint/eslint-plugin-template';
 import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
@@ -25,12 +24,24 @@ export default [
       'prettier/prettier': ['error', { endOfLine: 'auto', printWidth: 100 }],
       '@angular-eslint/component-selector': [
         'error',
-        { prefix: 'app', style: 'kebab-case', type: 'element' },
+        { prefix: 'ba', style: 'kebab-case', type: 'element' },
       ],
       '@angular-eslint/directive-selector': [
         'error',
-        { prefix: 'app', style: 'camelCase', type: 'attribute' },
+        { prefix: 'ba', style: 'camelCase', type: 'attribute' },
       ],
+      '@angular-eslint/contextual-lifecycle': 'error',
+      '@angular-eslint/no-empty-lifecycle-method': 'error',
+      '@angular-eslint/component-class-suffix': 'error',
+      '@angular-eslint/directive-selector': 'error',
+      '@angular-eslint/no-input-rename': 'error',
+      '@angular-eslint/no-output-native': 'error',
+      '@angular-eslint/no-inputs-metadata-property': 'error',
+      '@angular-eslint/no-output-on-prefix': 'error',
+      '@angular-eslint/no-output-rename': 'error',
+      '@angular-eslint/no-outputs-metadata-property': 'error',
+      '@angular-eslint/prefer-standalone': 'error',
+      '@angular-eslint/use-pipe-transform-interface': 'error',
       'arrow-body-style': 'off',
       'prefer-arrow-callback': 'off',
       '@typescript-eslint/no-namespace': 'off',
@@ -43,6 +54,9 @@ export default [
       prettier: prettierPlugin,
     },
     rules: {
+      '@angular-eslint/template/banana-in-box': 'error',
+      '@angular-eslint/template/eqeqeq': 'error',
+      '@angular-eslint/template/no-negated-async': 'error',
       'prettier/prettier': ['error'],
     },
   },
