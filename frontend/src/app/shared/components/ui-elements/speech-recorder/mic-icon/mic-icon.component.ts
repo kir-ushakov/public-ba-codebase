65797397f662b10571b71e504645976d9ac840c4
Kirill Ushakov <kitushakoff@gmail.com>
2025-05-19 11:52:45 +0300

    removed standlone

--- frontend/src/app/shared/components/ui-elements/speech-recorder/mic-icon/mic-icon.component.ts
+++ frontend/src/app/shared/components/ui-elements/speech-recorder/mic-icon/mic-icon.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ba-mic-icon',
-  standalone: true,
   imports: [],
   templateUrl: './mic-icon.component.html',
   styleUrl: './mic-icon.component.scss',
