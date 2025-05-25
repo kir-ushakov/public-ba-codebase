5d301ca1ab927da060430ac2bff2bb453f4ac6dd
Kirill Ushakov <kitushakoff@gmail.com>
2025-05-25 14:38:53 +0300

    fixed inject sevice error

--- frontend/src/app/shared/services/api/speech-to-text.service.ts
+++ frontend/src/app/shared/services/api/speech-to-text.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import type { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import type { Observable } from 'rxjs';
 import { delay, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
