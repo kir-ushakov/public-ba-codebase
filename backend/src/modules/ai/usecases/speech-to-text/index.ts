7fc83092170c632cfcf3b35e914d3013692461dd
Kirill Ushakov <kitushakoff@gmail.com>
2025-06-01 19:14:55 +0300

    update speech-to-text index with new dependncies

--- backend/src/modules/ai/usecases/speech-to-text/index.ts
+++ backend/src/modules/ai/usecases/speech-to-text/index.ts
@@ -1,10 +1,14 @@
+import { AudioPreprocessorService } from '../../../../shared/services/audio/audio-preprocessor.service.js';
+import { LoggerService } from '../../../../shared/services/logger/logger.service.js';
 import { OpenAIService } from '../../services/open-ai.service.js';
 import { SpeechToTextController } from './speech-to-text.controller.js';
 import { SpeechToText } from './speech-to-text.usecase.js';
 
 const OPEN_AI_API_KEY = process.env.OPEN_AI_API_KEY;
-const openAIService = new OpenAIService(OPEN_AI_API_KEY);
+const audioPreprocessorService = new AudioPreprocessorService();
+const openAIService = new OpenAIService(OPEN_AI_API_KEY, audioPreprocessorService);
 const speechToText = new SpeechToText(openAIService);
-const speechToTextController = new SpeechToTextController(speechToText);
+const loggerService = new LoggerService();
+const speechToTextController = new SpeechToTextController(speechToText, loggerService);
 
 export { speechToTextController };
