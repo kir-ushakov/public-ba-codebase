14e90d0630c7220cd0deb35b575107634948a32e
Kirill Ushakov <kitushakoff@gmail.com>
2025-06-01 19:10:36 +0300

    added  public readonly cause?: ServiceError to UseCaseError

--- backend/src/shared/core/use-case-error.ts
+++ backend/src/shared/core/use-case-error.ts
@@ -1,11 +1,13 @@
 import { EHttpStatus } from '../infra/http/models/base-controller.js';
 import { AppError } from './app-error.js';
+import { ServiceError } from './service-error.js';
 
 export class UseCaseError<U extends string> extends AppError<U> {
   constructor(
     code: U,
     message: string,
     public readonly httpCode: EHttpStatus,
+    public readonly cause?: ServiceError,
   ) {
     super(message, code);
   }
