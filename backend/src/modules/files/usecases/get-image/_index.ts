378d5c96a49ddbd991ea9c920b2da30c8980fd4e
Kirill Ushakov <kitushakoff@gmail.com>
2025-09-13 15:19:23 +0300

    inject imageRepoService

--- backend/src/modules/files/usecases/get-image/_index.ts
+++ backend/src/modules/files/usecases/get-image/_index.ts
@@ -2,8 +2,16 @@ import { GetImageController } from './get-image.controller.js';
 import { GetImageUsecase } from './get-image.usecase.js';
 import { googleDriveService } from './../../../integrations/google/services/index.js';
 import { imageResizeService } from '../../services/_index.js';
+import { ImageRepoService } from '../../../../shared/repo/image-repo.service.js';
+import { models } from '../../../../shared/infra/database/mongodb/index.js';
 
-const getImageUsecase = new GetImageUsecase(googleDriveService, imageResizeService);
+const imageRepoService = new ImageRepoService(models);
+
+const getImageUsecase = new GetImageUsecase(
+  googleDriveService,
+  imageResizeService,
+  imageRepoService,
+);
 const getImageController = new GetImageController(getImageUsecase);
 
 export { getImageController };
--- backend/src/modules/files/usecases/upload-image/_index.ts
+++ backend/src/modules/files/usecases/upload-image/_index.ts
@@ -1,7 +1,10 @@
 import { UploadImageController } from './upload-image.controller.js';
 import { UploadImageUsecase } from './upload-image.usecase.js';
 import { googleDriveService } from './../../../integrations/google/services/index.js';
+import { ImageRepoService } from '../../../../shared/repo/image-repo.service.js';
+import { models } from '../../../../shared/infra/database/mongodb/index.js';
 
-const uploadImageUsecase = new UploadImageUsecase(googleDriveService);
+const imageRepoService = new ImageRepoService(models);
+const uploadImageUsecase = new UploadImageUsecase(googleDriveService, imageRepoService);
 const uploadImageController = new UploadImageController(uploadImageUsecase);
 export { uploadImageController };
