cfe37bbe0a1122a095d7f6d2f4f91ea513fbe785
Kirill Ushakov <kitushakoff@gmail.com>
2025-09-21 10:26:30 +0300

    resizeImageToMaxSize adde to ImageResizeService

--- backend/src/modules/files/services/image-resize.service.ts
+++ backend/src/modules/files/services/image-resize.service.ts
@@ -1,6 +1,7 @@
 import sharp from 'sharp';
 import { Readable, PassThrough } from 'stream';
 import probe from 'probe-image-size';
+import { promises as fsp } from 'fs';
 
 export class ImageResizeService {
   public async resizeImage(
@@ -35,5 +36,24 @@ export class ImageResizeService {
     const contentType = mime;
 
     return { resized, contentType };
+  }
+
+  public async resizeImageToMaxSize(filePath: string, maxSiz: number): Promise<void> {
+    try {
+      // Resize image to a maximum of 1000px width or height (whichever is larger)
+      // while maintaining the aspect ratio
+      await sharp(filePath)
+        .resize(maxSiz, maxSiz, {
+          fit: 'inside',
+          withoutEnlargement: true, // Don't enlarge images smaller than MAX_IMAGE_WIDTH
+        })
+        .toFile(`${filePath}.tmp`);
+
+      // Replace the original file with the resized one
+      await fsp.rename(`${filePath}.tmp`, filePath);
+    } catch (error) {
+      console.error('Error resizing image:', error);
+      throw error;
+    }
   }
 }
