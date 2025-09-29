d1febb4cf41b829b973f54b21efecc37f546a8a6
Kirill Ushakov <kit.ushakoff@gmail.com>
2025-09-29 16:53:36 +0300

    Handle cancelation of taking photo the right way

--- frontend/src/app/shared/services/pwa/device-camera.service.ts
+++ frontend/src/app/shared/services/pwa/device-camera.service.ts
@@ -9,20 +9,16 @@ export class DeviceCameraService {
     if (quality < 0 || quality > 100) {
       throw new Error('Quality must be between 0 and 100');
     }
-
-    // TODO: handle case when user cancels the photo taking
-    // Now it lead to throw error - that never handled upper
-    // console.error('Error taking picture:', error);
-    // But in fact it's not an error
-    // TICKET: https://brainas.atlassian.net/browse/BA-244
+    
     try {
       const image = await Camera.getPhoto({
         quality,
         resultType: CameraResultType.Uri,
       });
 
-      if (!image.webPath) {
-        throw new Error('No image URL available');
+      // Cancel: no image selected
+      if (!image?.webPath) {
+        return null;
       }
 
       const imageUrl = image.webPath;
