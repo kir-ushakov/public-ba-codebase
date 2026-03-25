6379de00060c24ad694123f8b4a6ac03c7bd3fbd
Kirill Ushakov <kit.ushakoff@gmail.com>
2026-03-25 17:35:58 +0300

    refactoring

--- backend/src/modules/sync/usecases/release-client-id/release-client-id.dto.ts
+++ backend/src/modules/sync/usecases/release-client-id/release-client-id.dto.ts
@@ -1,8 +1,3 @@
-export interface IReleaseClientIdRequestDTO {
-  userId: string; 
-  // TODO: potentially have here type of device: `deviceId entityObject` or `deviceId entity String`
-}
-
-export interface  IReleaseClientIdResponseDTO {
+export interface IReleaseClientIdResponseDTO {
  clientId: string;
 }
